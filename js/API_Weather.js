0fbe3f09156e14f8d28b20f693a9bc3c669611cc
Arnaud MEILHOC <arnaud.meilhoc@edu.ece.fr>
2021-11-01 14:05:54 +0100

    final version

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,7 +3,7 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
@@ -11,7 +11,7 @@ const API_URL_ICON = "http://openweathermap.org/img/wn/";
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    if(city === ""){
       city = "paris";
     }
     this.city = city;
@@ -19,9 +19,26 @@ class API_WEATHER{
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
-  fetchTodayForecast(){
+  fetchDayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+    // on a rajouté cnt=4 dans la requête GET, comme décrit dans la documentation de l'api cela nous
+    // permet de récupérer la météo pour 4 jours, les trois prochains  plus aujourd'hui.
+    // On sépare par & les différentes variables car c'est la norme pour les discerner les uns des autres
+    // ainsi il existe plusieurs variables que on envoie à l'API comme la ville, l'unité (ici les mètres)
+    // le nombre de jours (cnt) et la clef qui permet de vérifier notre identité
+    //
+    // En gros dans l'url le "?" permet de dire que la query string commence et le "&" permet de séparer les variables
+    // en gros y'a des url qui mènent à des scritps qui sont sur le serveur. Ces scripts peuvent avoir besoin d'infos
+    // en mode ce que t'as rentré comme info avant de cliquer sur le lien, ces variables sont définies après le ?
+    // exemple je recherche ECE sur google, je rentre ECE dans la barre de recherche google et quand je clic ça m'amène sur
+    // l'url suivante :
+    // https://www.google.com/search?q=ECE&rlz=1C2CBBF_frFR822FR812&oq=ECE&aqs=chrome ...
+    // j'ai coupé la fin mais t'as capté y'a une première variable : q qui a comme valeur "ECE" une autre variable rlz qui a
+    // comme valeur "1C2CBBF_frFR822FR812" etc
+    // donc ici l'API faut lui passer des infos et c'est le meme principe
+    // pour savoir ce qu'il faut lui passer on va sur : "https://openweathermap.org/forecast16"
+    // y'a les infos
+    .get(`${API_URL}?q=${this.city}&units=metric&cnt=4&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-
 // Fonction appelée lors du click du bouton
 function start() {
-  // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+
+  const city=document.getElementById("city-input").value;
+  // Création de l'objet apiWeather 
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
-  apiWeather
-    .fetchTodayForecast()
-    .then(function(response) {
+  apiWeather.fetchDayForecast().then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
+      console.error(data)
+
+      for(let i=0;i<4;i++)
+      {
       // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      const main = data.list[i].weather[0].main;
+      const description = data.list[i].weather[0].description;
+      const temp = data.list[i].temp.day;
+      const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      document.getElementById(i+'-forecast-main').innerHTML = main;
+      document.getElementById(i+'-forecast-more-info').innerHTML = description;
+      document.getElementById(i+'-weather-container').innerHTML = icon;
+      document.getElementById(i+'-forecast-temp').innerHTML = `${temp}°C`;
       
-    })
-    .catch(function(error) {
-      // Affiche une erreur
-      console.error(error);
-    });
+    }
+  })
 }
